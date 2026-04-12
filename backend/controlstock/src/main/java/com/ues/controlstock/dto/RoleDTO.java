aec19add28267fa0ef1f672b1b025f3b62b30bcb
Yohalmo Castro <CS24009@ues.edu.sv>
2026-04-11 22:06:33 -0600

    refactor: anotaciones lombok a Role y RoleDTO

--- backend/controlstock/src/main/java/com/ues/controlstock/dto/RoleDTO.java
+++ backend/controlstock/src/main/java/com/ues/controlstock/dto/RoleDTO.java
@@ -1,20 +1,17 @@
 package com.ues.controlstock.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoleDTO {
     private Long id;
     private String roleName;
     private String description;
-
-    public Long getId() { return id; }
-
-    public void setId(Long id) { this.id = id; }
-
-    public String getRoleName() { return roleName; }
-
-    public void setRoleName(String roleName) { this.roleName = roleName; }
-
-    public String getDescription() { return description; }
-
-    public void setDescription(String description) { this.description = description; }
-  
 }

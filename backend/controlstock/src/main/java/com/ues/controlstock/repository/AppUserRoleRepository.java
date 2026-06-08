b988dfa51ecfa85490f5dbd74fd38d6c569d293a
ID24001 <id24001@ues.edu.sv>
2026-06-08 09:29:11 -0600

    fix(backend): validar asignacion de rol, si usuario ya tiene rol

--- backend/controlstock/src/main/java/com/ues/controlstock/repository/AppUserRoleRepository.java
+++ backend/controlstock/src/main/java/com/ues/controlstock/repository/AppUserRoleRepository.java
@@ -1,10 +1,19 @@
 package com.ues.controlstock.repository;
 
 import java.util.List;
+import java.util.Optional;
+
+import com.ues.controlstock.entity.AppUser;
+import org.hibernate.annotations.SQLSelect;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ues.controlstock.entity.AppUserRole;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AppUserRoleRepository extends JpaRepository<AppUserRole, Long> {
     List<AppUserRole> findByAppUser_UserId(Long userId);
-    List<AppUserRole> findByRole_RoleId(Long roleId);
+    
+    @Query(value = "SELECT * FROM app_user_role WHERE role_id = ?1 AND app_user_id = ?2", nativeQuery = true)
+    AppUserRole findByRoleIdAndAppUserId(Long roleId, Long appUserId);
+
 }
